7d68ca4d4eea209525297203e4d05a44d6831f58
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2026-07-11 16:43:40 +0530

    Add header section
    - Add separate folders for assets and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/chef_claude_icon.png'
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+      <h1> Chef Claude </h1>
+    </header>
+  )
+}

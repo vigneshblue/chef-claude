5c8f50a47da2009d8659c600523d622fdd80e875
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2026-07-12 08:20:16 +0530

    Add get recipe layout and conditional redering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,15 +6,13 @@ export default function Main() {
     <li key={ingredient}>{ingredient}</li>
   ))
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient")
     setIngredients(prevIngredients => [...prevIngredients, newIngredient])
   }
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input 
             type="text"
             placeholder="e.g. oregano"
@@ -23,9 +21,17 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-        <ul>
-          {ingredientsElements}
-        </ul>
+      {ingredients.length > 0 && <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">{ingredientsElements}</ul>
+        {ingredients.length > 3 && <div className="get-recipe-container">
+            <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+        </div>}
+      </section>}
     </main>
   )
 }
